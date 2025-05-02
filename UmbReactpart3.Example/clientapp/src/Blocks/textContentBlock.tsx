2ba85f842f4e39c1f7c1336d144b6d2e28915a61
Greg Fyans <gregfyans@gmail.com>
2025-05-02 10:35:45 +0100

    Tidy-up

--- UmbReactpart3.Example/clientapp/src/Blocks/textContentBlock.tsx
+++ UmbReactpart3.Example/clientapp/src/Blocks/textContentBlock.tsx
@@ -10,39 +10,13 @@ function TextContentBlock({ content }: { content: RichText }) {
 
     const contentClickHandler = (e: any) => {
         const targetLink = e.target.closest("a");
-
-        console.log("click")
-
         if (!targetLink) return;
-
         e.preventDefault();
-
         navigate(e.target.pathname);
     };
 
     return <div onClick={contentClickHandler} dangerouslySetInnerHTML={{ __html: content.textContent.markup }} />;
 }
-
-// Function to parse and transform the markup into JSX elements
-const parseMarkupToJSX = (markup: string) => {
-    return <div id="text-content-block" dangerouslySetInnerHTML={{ __html: markup }} />;
-
-    const navigate = useNavigate();
-
-    const contentClickHandler = (e: any) => {
-        const targetLink = e.target.closest("a");
-
-        console.log("click")
-
-        if (!targetLink) return;
-
-        e.preventDefault();
-
-        navigate(e.target.pathname);
-    };
-
-    return <div id="text-content-block" onClick={contentClickHandler} dangerouslySetInnerHTML={{ __html: markup }} />;
-};
 
 type RichText = {
     textContent: UmbracoRichText
